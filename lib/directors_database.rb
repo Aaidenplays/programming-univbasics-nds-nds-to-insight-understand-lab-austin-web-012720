c4361dc54a8d2adb3e50a965009e9255e0cc5d37
aaiden Nicolas Witten <aaidenplays@yahoo.com>
2020-01-03 00:47:38 +0000

    Automatically backed up by Learn

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -4,12 +4,5 @@ def directors_database
 	rot13 = -> (s) { s.tr('A-Za-z', 'N-ZA-Mn-za-m') }
   @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
 end
-    movies = directors_database[0][:movies]
-    
-    index = 0 
-    while index < movies.size do
-    puts movies[index][:title]
-    binding.pry       
-    index += 1
-    end
+
     
